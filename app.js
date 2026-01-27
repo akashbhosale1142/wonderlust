c49dcc3ee5418b145707bf1822005c94d5344b55
Akash <akash@example.com>
2026-01-28 01:00:42 +0530

    use removed

--- app.js
+++ app.js
@@ -44,7 +44,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static("public"));
 
 const store = MongoStore.create({
   mongoUrl: db_url,
